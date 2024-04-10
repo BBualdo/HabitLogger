d1f5ec435d0b18fda1c9020185c9a6c4df610bad
Sebastian <mrseb10@gmail.com>
2024-04-10 08:05:40 +0200

    Installing Sqlite NuGet Package and creating DbContext class

---
+++ DatabaseLibrary/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace DatabaseLibrary
+{
+  public class DbContext
+  {
+    private SqliteConnection _Connection { get; set; }
+
+    public DbContext()
+    {
+      _Connection = new SqliteConnection("Data Source=habits.db");
+    }
+  }
+}
