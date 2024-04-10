0133dd04586a18600c77f795b9b5b76c699fd9a0
Sebastian <mrseb10@gmail.com>
2024-04-10 08:12:10 +0200

    Create Tables method

--- DatabaseLibrary/DbContext.cs
+++ DatabaseLibrary/DbContext.cs
@@ -9,6 +9,34 @@ namespace DatabaseLibrary
     public DbContext()
     {
       _Connection = new SqliteConnection("Data Source=habits.db");
+      CreateTables();
+    }
+
+    private void CreateTables()
+    {
+      using (_Connection)
+      {
+        _Connection.Open();
+
+        string createTablesQuery = @"CREATE TABLE IF NOT EXISTS habit(
+                                    habit_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    name TEXT,
+                                    unit TEXT);
+                                    CREATE TABLE IF NOT EXISTS record(
+                                    record_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    habit_id INTEGER,
+                                    date TEXT,
+                                    quantity INTEGER,
+                                    FOREIGN KEY(habit_id) REFERENCES habit(habit_id)
+                                    )";
+
+        using (SqliteCommand command = new SqliteCommand(createTablesQuery, _Connection))
+        {
+          command.ExecuteNonQuery();
+        }
+
+        _Connection.Close();
+      }
     }
   }
 }
